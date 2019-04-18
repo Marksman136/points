9460e264a216942f9368964e58c0faf1f6d2fa2f
unknown <kalnins.marks@gmail.com>
2019-04-18 17:33:12 +0300

    punkti array

--- sketch.js
+++ sketch.js
@@ -1,14 +1,71 @@
+//point = [];
+let point;
+let x;
+let y;
+let speed;
+let proximity;
+
+
 function setup (){
-createCanvas(100,800);
-background(100);
+createCanvas(800,800);
+background(150, 200, 200);
+
+//for(let i = 0; i<200; i++){
+//point[i] = (new Points(random(0,800), random(0,800)));
+//}
+
+point = new Points(random(0,800),random(0,800), 0.02, 50);
 }
+
+
 
 function draw() {
 
-for(let i = 20; i< width, i+= 50);
+//for(let i = 0; i<point.length;i++ ){
+//point.display();
+//
+//}
 
-
-ellipse(i,20,10,10);
+point.display();
+point.update();
 }
+
+
+
+class Points {
+
+constructor (x, y, speed, proximity) {
+this.x = x;
+this.y = y;
+this.speed = speed;
+this.proximity = proximity;
+this.xoff = random(5000);
+this.yoff = random(5000);
+
+
+}
+display(){
+
+fill(0,220,100, 75);
+ellipse(this.x, this.y, 50, 50);
+noStroke();
+
+
+}
+
+update(){
+
+this.x = noise(this.xoff) * (height);
+this.x = this.x + width/6;
+this.xoff += this.speed;
+
+this.y = noise(this.yoff) * (height);
+this.y = this.y + width/6;
+this.yoff += this.speed;
+
+
+
+}
+
 
 }
