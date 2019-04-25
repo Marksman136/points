6e02777b49e3d89afa1deb3d7ec5bc4bf30db288
unknown <kalnins.marks@gmail.com>
2019-04-25 17:43:15 +0300

    final

--- sketch.js
+++ sketch.js
@@ -1,70 +1,80 @@
-//point = [];
-let point;
 let x;
 let y;
 let speed;
 let proximity;
+other = [];
+points = [];
 
 
-function setup (){
-createCanvas(800,800);
-background(150, 200, 200);
 
-//for(let i = 0; i<200; i++){
-//point[i] = (new Points(random(0,800), random(0,800)));
-//}
+function setup() {
+  createCanvas(1200, 600);
+  for (let i = 0; i < 20; i++) {
+    points.push(new Points(random(0,1200),random(0,600), 0.005, 100));
 
-point = new Points(random(0,800),random(0,800), 0.02, 50);
+  }
 }
-
 
 
 function draw() {
-
-//for(let i = 0; i<point.length;i++ ){
-//point.display();
-//
-//}
-
-point.display();
-point.update();
-}
+background(0);
+strokeWeight(2);
 
 
-
-class Points {
-
-constructor (x, y, speed, proximity) {
-this.x = x;
-this.y = y;
-this.speed = speed;
-this.proximity = proximity;
-this.xoff = random(5000);
-this.yoff = random(5000);
+stroke(150,30,100);
+fill(110,50,200, 100);
 
 
-}
-display(){
+// for (let i = 0; i < points.length; i++) {
+//   points[i].display();
+// }
 
-fill(0,220,100, 75);
-ellipse(this.x, this.y, 50, 50);
-noStroke();
 
+for (let i = 0; i < points.length; i++) {
+
+//1.SOLIS - NEKUSTĪGI OBJEKTI
+points[i].display();
+points[i].update();
+
+//2.SOLIS - KUSTĪGI OBJEKTI
+
+for (let j = 0; j < points.length; j++) {
+if (i != j && points[i].isNear(points[j])) {
+line(points[i].x, points[i].y, points[j].x, points[j].y);
 
 }
+}
+}
 
-update(){
+}
+class Points{
+  constructor(x, y, speed, proximity){
 
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.proximity = proximity;
+  this.xoff = random(50000);
+  this.yoff = random(50000);
+  }
+
+  display(){
+
+  ellipse(this.x,this.y, 40, 40);
+  }
+
+
+update() {
 this.x = noise(this.xoff) * (height);
-this.x = this.x + width/6;
+this.x += width/4; // this is here just to offset (to "center") the x coordinate.
 this.xoff += this.speed;
-
 this.y = noise(this.yoff) * (height);
-this.y = this.y + width/6;
 this.yoff += this.speed;
+}
 
-
-
+isNear(other) {
+let d = dist(this.x, this.y, other.x, other.y);
+return d < this.proximity;
 }
 
 
